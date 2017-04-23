bc044629f3022eec0897b994eefcdc82b6620503
danilov_sergey <sergey.event@gmail.com>
2017-04-23 20:10:09 +0300

    view and js for search results

--- assets/js/search_by_word.js
+++ assets/js/search_by_word.js
@@ -1,13 +1,35 @@
-document.getElementById('search_input_field').addEventListener('keyup', function(){
-	if(this.value.length){
-		displaySearchResult(searchPerform(this.value), 5)
-	} else {
-		document.getElementById('search_results').innerHTML = ''
+previewSearchResults()
+searchPageResults()
+
+function searchPageResults(){
+	if(window.location.href.includes('search')){
+		q = getSearchQueryVariable('query')
+		displaySearchResultForPage(searchPerform(q))
 	}
-})
+}
+
+
+function getSearchQueryVariable(variable){
+  var query = window.location.search.substring(1)
+  var vars = query.split('&')
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=')
+    if (pair[0] === variable) { return decodeURIComponent(pair[1].replace(/\+/g, '%20')) }
+  }
+}
+
+function previewSearchResults(){
+	document.getElementById('search_input_field').addEventListener('keyup', function(){
+		if(this.value.length){
+			displaySearchResult(searchPerform(this.value), 5)
+		} else {
+			document.getElementById('search_results').innerHTML = ''
+		}
+	})
+}
 
 function searchPerform(search_word){
-	var results    = []
+	var results = []
 
 	for(var post in window.store){
 		if(search_word !== '' && window.store[post].title.toLowerCase().includes(search_word.toLowerCase())){
@@ -23,10 +45,22 @@ function searchPerform(search_word){
 function displaySearchResult(results, maxCount){
 	var searchResults = document.getElementById('search_results')
 	var appendString = ''
-
 	if(results.length){
 		for(var i = 0; i < results.length && i < maxCount; i++){
 			appendString += '<li><a href = "' + results[i].url + '"><p>'+ results[i].title +'</p></a></li>'
+		}
+		searchResults.innerHTML = appendString
+	} else {
+		searchResults.innerHTML = '<b>Совпадений нет</b>'
+	}
+}
+
+function displaySearchResultForPage(results){
+	var searchResults = document.getElementById('result_search_el_list')
+	var appendString = ''
+	if(results.length){
+		for(var i = 0; i < results.length; i++){
+			appendString += '<a href = "' + results[i].url + '"><li>'+ results[i].title +'</li></a>'
 		}
 		searchResults.innerHTML = appendString
 	} else {
--- assets/js/search_old.js
+++ assets/js/search_old.js
@@ -1,14 +1,14 @@
-// get search word
+get search word
 
-// function getSearchQueryVariable(variable){
-//   var query = window.location.search.substring(1)
-//   var vars = query.split('&')
+function getSearchQueryVariable(variable){
+  var query = window.location.search.substring(1)
+  var vars = query.split('&')
 
-//   for (var i = 0; i < vars.length; i++) {
-//     var pair = vars[i].split('=')
-//     if (pair[0] === variable) { return decodeURIComponent(pair[1].replace(/\+/g, '%20')) }
-//   }
-// }
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=')
+    if (pair[0] === variable) { return decodeURIComponent(pair[1].replace(/\+/g, '%20')) }
+  }
+}
 
 function searchPerform(search_word){
 	var searchWord = search_word
