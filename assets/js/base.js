c968fa77a61ab6c8c48170bb8e36292f58687850
danilov_sergey <sergey.event@gmail.com>
2017-06-03 20:47:17 +0300

    ajax for index post page

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,7 +1,91 @@
+var serverAddress = 'https://shrouded-ravine-70922.herokuapp.com/'
+
 activeHeaderMenuElement()
+sendPostsIdsForCurrentPage()
+
 
 function activeHeaderMenuElement(){
 	currentUrl = window.location.pathname
 	if (currentUrl.includes('blog')){ document.getElementById('blog_root').className += 'active'}
 	if (currentUrl.includes('about')){ document.getElementById('about_root').className += 'active'}
+}
+
+
+function sendPostsIdsForCurrentPage(){
+	posts = document.querySelectorAll('[data-post-id]')
+	postsIds = []
+
+	for(var i = 0 ; i < posts.length ; i++){
+		postsIds.push(posts[i].dataset.postId)
+	}
+
+	queryPostDataForCurrentPage(postsIds)
+}
+
+
+function queryPostDataForCurrentPage(postsIds){
+	var xhrObject = new XMLHttpRequest()
+	xhrObject.open('POST', serverAddress + 'posts_info.json', true)
+
+	arr = postsIds
+	res = JSON.stringify(arr)
+
+	var formData = new FormData()
+	formData.append('posts_ids', res)
+	xhrObject.send(formData)
+
+	xhrObject.onload = function(){
+		handlePostDataForCurrentPage(postsIds, JSON.parse(xhrObject.responseText))
+	}
+
+	xhrObject.onerror = function(){
+		console.log('Error: ' + this.status + ' method getPostsInfo failed')
+	}
+}
+
+
+function handlePostDataForCurrentPage(postsIds, postsDataFromServer){
+	res = []
+	existIds = postsDataFromServer.map(function(item){
+		return item['uid']
+	})
+
+	for(var i = 0 ; i < postsIds.length ; i++){
+		if(existIds.includes(postsIds[i])){
+
+			post = postsDataFromServer.filter(function(item){ 
+				return item['uid'] === postsIds[i] 
+			})[0]
+
+			viewsCount = post.views_count
+			commetsCount = post.comments_count
+
+			res.push({
+				id: postsIds[i],
+				viewCount: viewsCount,
+				commentCount: commetsCount
+			})
+		} else {
+			res.push({
+				id: postsIds[i],
+				viewCount: 0,
+				commentCount: 0
+			})
+		}
+	}
+	displayPostDataForCurrentPage(res)
+}
+
+
+function displayPostDataForCurrentPage(finalPostArr){
+	posts = document.querySelectorAll('[data-post-id]')
+
+	for(var i = 0 ; i < posts.length ; i++){
+		postId = posts[i].dataset.postId
+		post = finalPostArr.filter(function(obj){ return obj.id === postId })[0]
+
+		pairCommentView = posts[i].querySelectorAll('.posts_el_info_cnt li')
+		commentCnt = pairCommentView[0].querySelector('span').innerText = post.commentCount
+		viewCnt = pairCommentView[1].querySelector('span').innerText = post.viewCount
+	}
 }
--- assets/js/post.js
+++ assets/js/post.js
@@ -1,8 +1,34 @@
-
 var serverAddress = 'https://shrouded-ravine-70922.herokuapp.com/'
+var postId = document.getElementsByName('post_id')[0].value
+
+writeUserIpToLocalStorage()
 initCommentData()
 queryGetPostComments()
 handleCommentBtn()
+createPostView(postId, localStorage.userIP)
+getPostInfo(postId)
+
+
+// COMMENTS
+
+
+function writeUserIpToLocalStorage(){
+	var xhrObject = new XMLHttpRequest()
+
+	xhrObject.open('GET', 'https://freegeoip.net/json/', true)
+	xhrObject.send()
+
+	xhrObject.onload = function(){
+		json = JSON.parse(xhrObject.responseText)
+		ip = json['ip']
+		localStorage.userIP = ip
+	}
+
+	xhrObject.onerror = function(){
+		console.log('Error: ' + this.status + ' method myIP failed')
+	}
+}
+
 
 function dateFormatForComment(date){
 	d = date.substring(8, 10)
@@ -28,11 +54,13 @@ function dateFormatForComment(date){
 	return res
 }
 
+
 function initCommentData(){
 	if(localStorage.localUserId !== undefined){
-		document.getElementsByName('user_id')[0].setAttribute('value', localStorage.localUserId)
+		document.getElementsByName('user_id')[0].setAttribute('value', '1')
 	}
 }
+
 
 function queryGetPostComments(){
 	var xhrObject = new XMLHttpRequest()
@@ -49,10 +77,10 @@ function queryGetPostComments(){
 	}
 }
 
+
 function displayPostComments(inputCommentsString){
 	comments = JSON.parse(inputCommentsString)
 	if(!comments.length){return}
-	console.log(comments.length)
 	commentsCnt = document.getElementById('post_comments')
 	res = []
 	for(var i = 0; i < comments.length; i++){
@@ -72,6 +100,7 @@ function displayPostComments(inputCommentsString){
 	commentsCnt.innerHTML = res
 }
 
+
 function handleCommentBtn(){
 	btn = document.getElementById('send_comment_btn')	
 	btn.onclick = function(e){
@@ -82,31 +111,32 @@ function handleCommentBtn(){
 	}
 }
 
+
 function queryAddComment(){
 	var xhrObject = new XMLHttpRequest()
-
 	xhrObject.open('POST', serverAddress + 'create_comment.json', true)
-	// xhrObject.setRequestHeader('X-CSRF-Token', document.querySelector('meta[name="csrf-token"]').getAttribute('content'))
 
 	var formData = new FormData(document.forms.comment_form)
 	xhrObject.send(formData)
 
 	xhrObject.onload = function(){
+		document.getElementsByName('comment_body')[0].value = ''		
 		commentsCnt = document.getElementById('post_comments')
-		comment = JSON.parse(xhrObject.responseText)
+		obj = JSON.parse(xhrObject.responseText)
 
 		str = [
 			"<li>",
-				"<img src = '" + comment.picture_url + "'>",
+				"<img src = '" + obj['user'].picture_url + "'>",
 				"<div class = 'comment_cnt'>",
 					"<div class = 'comment_head'>",
-						"<div class = 'author_name'>" + comment.user_id + "</div>",
-						"<div class = 'comment_date'>" + dateFormatForComment(comment.created_at) + "</div>",
+						"<div class = 'author_name'>" + obj['user'].first_name + "</div>",
+						"<div class = 'comment_date'>" + dateFormatForComment(obj['comment'].created_at) + "</div>",
 					"</div>",
-					"<div class = 'comment_body'>" + comment.body + "</div>",
+					"<div class = 'comment_body'>" + obj['comment'].body + "</div>",
 				"</div>",
 			"</li>"
 		].join('')
+
 
 		commentsCnt.innerHTML += str
 	}
@@ -114,4 +144,38 @@ function queryAddComment(){
 	xhrObject.onerror = function(){
 		console.log('Error: ' + this.status + 'method queryAddComment failed')
 	}
+}
+
+
+// VIEWS
+
+
+function createPostView(postId, ip){
+	var xhrObject = new XMLHttpRequest()
+	xhrObject.open('POST', serverAddress + 'create_view.json', true)
+
+	var formData = new FormData()
+	formData.append('post_id', postId)
+	formData.append('ip', ip)
+	xhrObject.send(formData)
+
+	xhrObject.onload = function(){
+	}
+
+	xhrObject.onerror = function(){
+		console.log('Error: ' + this.status + ' method createPostView failed')
+	}
+}
+
+
+function getPostInfo(postId){
+	var xhrObject = new XMLHttpRequest()
+	xhrObject.open('GET', serverAddress + 'post_info.json?post_id=' + postId, true)
+	xhrObject.send()
+
+	xhrObject.onload = function(){}
+
+	xhrObject.onerror = function(){
+		console.log('Error: ' + this.status + 'method getPostInfo failed')
+	}	
 }
