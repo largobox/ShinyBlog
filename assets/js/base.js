a4da2ca2a246e37f0cb8f1b8464605d16d3dc5d1
danilov_sergey <sergey.event@gmail.com>
2017-06-16 20:37:41 +0300

    styles for header footer and about

--- assets/js/base.js
+++ assets/js/base.js
@@ -5,9 +5,11 @@ sendPostsIdsForCurrentPage()
 
 
 function activeHeaderMenuElement(){
+
 	currentUrl = window.location.pathname
-	if (currentUrl.includes('blog')){ document.getElementById('blog_root').className += 'active'}
-	if (currentUrl.includes('about')){ document.getElementById('about_root').className += 'active'}
+	console.log(currentUrl)
+	if (currentUrl === '/'){ document.getElementById('blog_root').className += 'active'}
+	if (currentUrl === '/about'){ document.getElementById('about_root').className += 'active'}
 }
 
 
